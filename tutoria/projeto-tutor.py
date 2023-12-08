493eeb038c14f2ee200c5862035a2f45d7e43096
Ernandes Tavares <131466882+NandoTavares@users.noreply.github.com>
2023-12-08 00:30:46 -0300

    Update projeto-tutor.py

--- tutoria/projeto-tutor.py
+++ tutoria/projeto-tutor.py
@@ -9,50 +9,59 @@ DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 intents = discord.Intents.default()
-intents.messages = True  # Habilitar permissões de mensagens
+intents.messages = True
+intents.guilds = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 openai.api_key = OPENAI_API_KEY
 
-async def buscar_historico_canal(canal, limit=2):
+async def buscar_historico_canal(canal, limit=5):
     messages_list = []
     async for message in canal.history(limit=limit):
-        messages_list.append(
-            {
-                "role": "user" if message.author.id != bot.user.id else "system",
-                "content": message.content
-            }
-        )
+        role = "user" if message.author.id != bot.user.id else "system"
+        content = message.content
+        messages_list.append(f"{role}: {content}")
 
     messages_list.reverse()
     return messages_list
 
-async def ask_gpt(mensagens):
-    response = await openai.ChatCompletion.create(
-        model="text-ada-001",
-        messages=mensagens,
-        temperature=0.9,
+def ask_gpt(mensagens):
+    prompt = "\n".join(mensagens)
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
         max_tokens=300
     )
 
-    return response['choices'][0]['message']['content']
+    resposta = response['choices'][0]['text'].replace("system:", "").strip()
+    return resposta
 
 @bot.event
 async def on_ready():
-    print(f"O {bot.user.name} ficou ligado!")
+    print(f"A {bot.user.name} ficou ligado!")
 
 @bot.event
 async def on_message(message):
-    if message.author.bot:
-        return
+    try:
+        if message.author.bot:
+            return
 
-    async with message.channel.typing():
-        mensagens = await buscar_historico_canal(message.channel)
-        resposta = await ask_gpt(mensagens)
+        async with message.channel.typing():
+            # Verificar se a mensagem começa com '\'
+            if message.content.startswith("\\"):
+                mensagens = await buscar_historico_canal(message.channel)
+                resposta = ask_gpt(mensagens)
 
-        await message.reply(resposta)
+                await message.reply(resposta)
 
-    await bot.process_commands(message)
+    except Exception as e:
+        print(f"Erro ao processar mensagem: {e}")
+        import traceback
+        traceback.print_exc()
+
+    finally:
+        await bot.process_commands(message)
 
 bot.run(DISCORD_BOT_TOKEN)
