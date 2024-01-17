8ae28e60d532e427b65d78344290176cab57ce8d
Ernandes Tavares <131466882+NandoTavares@users.noreply.github.com>
2024-01-17 17:18:43 -0300

    Update projeto-tutor.py

--- tutoria/projeto-tutor.py
+++ tutoria/projeto-tutor.py
@@ -1,10 +1,12 @@
 import discord
-from discord.ext import commands
 import openai
 from dotenv import load_dotenv
 import os
+from discord.ext import commands
+import logging
 
 load_dotenv()
+
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -13,7 +15,6 @@ intents.messages = True
 intents.guilds = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 openai.api_key = OPENAI_API_KEY
 
 async def buscar_historico_canal(canal, limit=5):
@@ -40,9 +41,12 @@ def ask_gpt(mensagens):
     resposta = response['choices'][0]['message']['content'].strip()
     return resposta
 
+
+
 @bot.event
 async def on_ready():
     print(f"A {bot.user.name} ficou ligado!")
+
 
 @bot.event
 async def on_message(message):
@@ -51,12 +55,12 @@ async def on_message(message):
             return
 
         async with message.channel.typing():
-            # Verificar se a mensagem começa com '\'
+            
             if message.content.startswith("\\"):
                 mensagens = await buscar_historico_canal(message.channel)
                 resposta = ask_gpt(mensagens)
 
-                # Verificar se a resposta não está vazia
+                
                 if resposta.strip():
                     await message.reply(resposta)
                 else:
@@ -69,5 +73,27 @@ async def on_message(message):
 
     finally:
         await bot.process_commands(message)
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    cog_directory = os.path.join(os.path.dirname(__file__), 'cogs')
+
+    if not os.path.exists(cog_directory):
+        print(f"Error: The 'cogs' directory '{cog_directory}' does not exist.")
+        return
+
+    for filename in os.listdir(cog_directory):
+        if filename.endswith('.py'):
+            try:
+                cog_path = f'cogs.{filename[:-3]}'
+                await bot.load_extension(cog_path)
+                print(f"Loaded cog: {cog_path}")
+            except Exception as e:
+                print(f"Failed to load cog {filename}: {e}")
+
+    TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+    await bot.start(TOKEN)
+
 
 bot.run(DISCORD_BOT_TOKEN)
